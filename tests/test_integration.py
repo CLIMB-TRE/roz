a2e6f319d9cd96dd8fd67623fb8ca36bd2b047d6
Biowilko <sam1.wilkinson1@gmail.com>
2023-10-13 13:01:10 +0100

    Fix last-last dummy filehandle

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -889,9 +889,7 @@ class Test_mscape_validator(unittest.TestCase):
         open(
             os.path.join(classifications_path, "PlusPF.kraken_report.txt"), "w"
         ).close()
-        open(
-            os.path.join(binned_reads_path, "reads_by_taxa/reads.286.fastq.gz"), "w"
-        ).close()
+        open(os.path.join(binned_reads_path, "reads.286.fastq.gz"), "w").close()
         open(
             os.path.join(DIR, f"{example_validator_message['uuid']}_report.pdf"), "w"
         ).close()
