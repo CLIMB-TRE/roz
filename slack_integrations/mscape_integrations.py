817e5f97d9f35f7092bd8762c851ddcde5090667
Biowilko <sam1.wilkinson1@gmail.com>
2023-10-25 13:27:27 +0100

    Finalise message formatting

--- slack_integrations/mscape_integrations.py
+++ slack_integrations/mscape_integrations.py
@@ -2,27 +2,32 @@ from varys import varys
 import os
 import requests
 import json
+import sys
 
 varys_client = varys(
     profile="roz",
     logfile=os.devnull,
     log_level="CRITICAL",
-    auto_acknowledge=True,
+    auto_acknowledge=False,
 )
 
 new_artifact_url = os.getenv("NEW_ARTIFACT_WEBHOOK")
+
+message_template = """*New MScape Artifact Published*
+```
+{}
+```
+"""
+
 
 while True:
     in_message = varys_client.receive(
         "inbound.new_artifact.mscape", queue_suffix="slack_integration"
     )
 
-    out_text = f"""*New MScape Artifact Published*
-                   Message:
-                   ```
-                   {json.dumps(in_message.body, indent=2)}
-                   ```
-                   """
+    in_dict = json.loads(in_message.body)
+
+    out_text = message_template.format(json.dumps(in_dict, indent=2))
 
     out_message = {"text": out_text}
 
@@ -30,3 +35,6 @@ while True:
 
     if not r.ok:
         print(f"Error posting to Slack webhook: {r.status_code} - {r.reason}")
+        sys.exit(1)
+
+    varys_client.acknowledge_message(in_message)
