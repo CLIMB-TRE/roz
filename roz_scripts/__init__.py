7a29fbc68f039ebcab9d8b1f8c2005fa272434ee
Biowilko <sam1.wilkinson1@gmail.com>
2024-01-18 12:50:17 +0000

    V1.2.1 version bump

--- roz_scripts/__init__.py
+++ roz_scripts/__init__.py
@@ -3,7 +3,4 @@ from .pathsafe import pathsafe_validation
 from .utils import utils
 from .general import ingest, s3_controller, s3_matcher, s3_notifications
 
-from get_version import get_version
-
-__version__ = get_version(__file__)
-del get_version
+__version__ = "1.2.1"
