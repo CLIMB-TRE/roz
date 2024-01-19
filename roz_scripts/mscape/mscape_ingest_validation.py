14c777fe7b8d3a898d5e873e9470739bdbf8f3ae
Biowilko <sam1.wilkinson1@gmail.com>
2024-01-19 14:02:34 +0000

    Reduce batch size to 100

--- roz_scripts/mscape/mscape_ingest_validation.py
+++ roz_scripts/mscape/mscape_ingest_validation.py
@@ -426,7 +426,7 @@ def add_classifier_calls(
         alert = True
 
     if not classifier_calls_fail:
-        for batch in batched(classifier_calls, 3000):
+        for batch in batched(classifier_calls, 100):
             update_fail, update_alert, payload = onyx_update(
                 payload=payload,
                 fields={"classifier_calls": batch},
