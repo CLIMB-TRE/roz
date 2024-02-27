6c8f3c870c9ff3c7d9bb9e0d640f93959b33f14f
Biowilko <sam1.wilkinson1@gmail.com>
2024-02-27 12:27:17 +0000

    Fix mscape slack integration

--- slack_integrations/mscape_integrations.py
+++ slack_integrations/mscape_integrations.py
@@ -62,7 +62,11 @@ while True:
     if public_result_message:
         in_dict = json.loads(public_result_message.body)
 
-        outcome = "Success" if in_dict.get("published") else "Failure"
+        outcome = ""
+
+        if type(in_dict) == dict:
+
+            outcome = "Success" if in_dict.get("published") else "Failure"
 
         out_text = public_result_message_template.format(
             outcome, json.dumps(in_dict, indent=2)
