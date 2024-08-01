a4b229a022969c46a5fcd039fc69f57b8db96868
Biowilko <sam1.wilkinson1@gmail.com>
2024-08-01 12:52:14 +0100

    Fix exit code parsing again

--- roz_scripts/mscape/mscape_ingest_validation.py
+++ roz_scripts/mscape/mscape_ingest_validation.py
@@ -1003,13 +1003,13 @@ def ret_0_parser(
                         "At least one FASTQ in the pair appear to not contain valid header lines, please resubmit valid FASTQ files or contact the mSCAPE admin team if you believe this to be in error"
                     )
                     ingest_fail = True
-                if process.startswith("paired_concatenate") and trace["exit"] == "8":
+                elif process.startswith("paired_concatenate") and trace["exit"] == "8":
                     payload.setdefault("ingest_errors", [])
                     payload["ingest_errors"].append(
-                        "Paired FASTQ files do not appear to match, please resubmit valid FASTQ files or contact the mSCAPE admin team if you believe this to be in error"
+                        "Paired FASTQ read headers do not appear to match between files, please resubmit valid FASTQ files or contact the mSCAPE admin team if you believe this to be in error"
                     )
                     ingest_fail = True
-                if (
+                elif (
                     process.startswith("extract_taxa_reads")
                     or process.startswith("extract_taxa_paired_reads")
                 ) and trace["exit"] == "2":
