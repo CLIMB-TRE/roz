77c62a581c8b8f412ff77502eefc42a908750473
Biowilko <sam1.wilkinson1@gmail.com>
2025-04-02 14:08:09 +0100

    Add s3 onyx updates and tests

--- roz_scripts/general/s3_onyx_updates.py
+++ roz_scripts/general/s3_onyx_updates.py
@@ -13,14 +13,12 @@ from roz_scripts.utils.utils import (
     onyx_update,
     s3_to_fh,
     csv_field_checks,
-    EtagMismatchError,
 )
 from roz_scripts.general.s3_matcher import parse_object_key
 from varys import Varys
 
 from onyx import (
     OnyxClient,
-    OnyxConfig,
 )
 
 from onyx.exceptions import (
@@ -30,10 +28,6 @@ from onyx.exceptions import (
     OnyxConfigError,
     OnyxClientError,
 )
-
-
-def csv_onyx_update(message, roz_config_dict, log):
-    pass
 
 
 def onyx_climb_identify(run_index: str, run_id: str, project: str, log):
@@ -53,15 +47,21 @@ def onyx_climb_identify(run_index: str, run_id: str, project: str, log):
                 )
 
                 if len(response) == 0:
-                    log.error(
+                    log.info(
                         f"Failed to find records with Onyx for: {project}.{run_index}.{run_id} despite successful identification by Onyx"
                     )
                     return (False, False)
 
                 else:
                     if response[0]["is_published"]:
+                        log.info(
+                            f"Successfully identified climb_id: {response[0]['climb_id']} for {project}.{run_index}.{run_id}"
+                        )
                         return (False, response[0]["climb_id"])
 
+                    log.info(
+                        f"Found record with Onyx for: {project}.{run_index}.{run_id} but not published therefore skipping update"
+                    )
                     return (False, False)
 
             except OnyxConnectionError as e:
@@ -97,7 +97,7 @@ def onyx_climb_identify(run_index: str, run_id: str, project: str, log):
                 return (True, False)
 
             except Exception as e:
-                log.error(f"Unhandled check_published error: {e}")
+                log.error(f"Unhandled onyx_climb_identify error: {e}")
                 return (True, False)
 
 
@@ -118,7 +118,11 @@ def onyx_identify_simple(
                     site=site,
                 )
 
-                return (True, response["identifier"])
+                log.info(
+                    f"Successfully identified {identity_field}: {identifier} as {response['identifier']}"
+                )
+
+                return (False, response["identifier"])
 
             except OnyxConnectionError as e:
                 if reconnect_count < 3:
@@ -134,30 +138,214 @@ def onyx_identify_simple(
                         f"Failed to connect to Onyx {reconnect_count} times with error: {e}"
                     )
 
-                    return (False, False)
+                    return (True, False)
 
             except (OnyxServerError, OnyxConfigError) as e:
                 log.error(f"Unhandled Onyx identify error: {e}")
-                return (False, False)
+                return (True, False)
 
             except OnyxClientError as e:
                 log.error(
                     f"Onyx identify failed for {identity_field}: {identifier} . Error: {e}"
                 )
-                return (False, False)
+                return (True, False)
 
             except OnyxRequestError as e:
                 if e.response.status_code == 404:
-                    return (True, False)
+                    return (False, False)
 
                 log.error(
                     f"Onyx identify failed for {identity_field}: {identifier}. Error: {e}"
                 )
-                return (False, False)
+                return (True, False)
 
             except Exception as e:
                 log.error(f"Unhandled onyx_identify error: {e}")
-                return (False, False)
+                return (True, False)
+
+
+def csv_update(parsed_messsage, config_dict, log):
+
+    record = parsed_messsage["Records"][0]
+    bucket_name = record["s3"]["bucket"]["name"]
+
+    parsed_bucket_name = {
+        x: y
+        for x, y in zip(
+            ("project", "site_str", "platform", "test_flag"),
+            bucket_name.split("-"),
+        )
+    }
+
+    if "." in parsed_bucket_name["site_str"]:
+        site = parsed_bucket_name["site_str"].split(".")[-2]
+    else:
+        site = parsed_bucket_name["site_str"]
+
+    # ignore files from test buckets
+    if parsed_bucket_name["test_flag"] == "test":
+        log.info(f"Ignoring file: {record["s3"]["object"]["key"]} from test bucket: {bucket_name}")
+        return (True, False)
+
+    # Skip new files from projects which do not have csv_updates enabled
+    if not config_dict["configs"][parsed_bucket_name["project"]]["csv_updates"]:
+        log.info(
+            f"Skipping file: {record['s3']['object']['key']} from project: {parsed_bucket_name['project']}"
+        )
+        return (True, False)
+
+    extension, parsed_object_key = parse_object_key(
+        object_key=record["s3"]["object"]["key"],
+        config_dict=config_dict,
+        project=parsed_bucket_name["project"],
+        platform=parsed_bucket_name["platform"],
+    )
+
+    if not extension or not parsed_object_key:
+        log.info(f"Failed to parse object key: {record['s3']['object']['key']}")
+        return (True, False)
+
+    if extension != ".csv":
+        log.info(
+            f"File: {record['s3']['object']['key']} is not a CSV file. Ignoring"
+        )
+        return (True, False)
+
+    run_index_fail, run_index = onyx_identify_simple(
+        identifier=parsed_object_key["run_index"],
+        identity_field="run_index",
+        project=parsed_bucket_name["project"],
+        site=site,
+        log=log,
+    )
+
+    if run_index_fail:
+        log.error(
+            f"Failed to identify run_index: {parsed_object_key['run_index']} for project: {parsed_bucket_name['project']}"
+        )
+        return (False, False)
+
+    run_id_fail, run_id = onyx_identify_simple(
+        identifier=parsed_object_key["run_id"],
+        identity_field="run_id",
+        project=parsed_bucket_name["project"],
+        site=site,
+        log=log,
+    )
+
+    if run_id_fail:
+        log.error(
+            f"Failed to identify run_id: {parsed_object_key['run_id']} for project: {parsed_bucket_name['project']}"
+        )
+        return (False, False)
+
+    if not all([run_id, run_index]):
+        log.info(f"Either run_id or run_index is unknown to onyx for {record['s3']['object']['key']}")
+        return (True, False)
+
+    climb_id_fail, climb_id = onyx_climb_identify(
+        run_index=run_index,
+        run_id=run_id,
+        project=parsed_bucket_name["project"],
+        log=log,
+    )
+
+    if climb_id_fail:
+        log.error(
+            f"Failed to identify climb_id for {parsed_bucket_name['project']}.{run_index}.{run_id}"
+        )
+        return (False, False)
+
+    if not climb_id:
+        log.info(
+            f"Climb_id not found for {parsed_bucket_name['project']}.{run_index}.{run_id}"
+        )
+        return (True, False)
+
+    payload = {
+        "climb_id": climb_id,
+        "project": parsed_bucket_name["project"],
+        "run_index": parsed_object_key["run_index"],
+        "run_id": parsed_object_key["run_id"],
+        "site": site,
+        "files": {
+            ".csv": {
+                "uri": f"s3://{bucket_name}/{record['s3']['object']['key']}",
+                "etag": record["s3"]["object"]["eTag"],
+                "key": record["s3"]["object"]["key"],
+            },
+        },
+        "artifact": f"{parsed_bucket_name['project']}.{parsed_object_key["run_index"]}.{parsed_object_key["run_id"]}",
+        "uuid": "fake_uuid",
+    }
+
+    field_check_status, alert, payload = csv_field_checks(payload=payload)
+
+    if alert:
+        log.error(
+            f"Field check failed for {parsed_bucket_name['project']}.{run_index}.{run_id}."
+        )
+        return (False, payload)
+
+    if not field_check_status:
+        log.info(
+            f"Field check failed: {payload}"
+        )
+        return (True, payload)
+
+    csv_s3_uri = f"s3://{bucket_name}/{record['s3']['object']['key']}"
+
+    with s3_to_fh(csv_s3_uri, record["s3"]["object"]["eTag"]) as fh:
+        reader = csv.DictReader(fh)
+
+        records = [record for record in reader]
+
+        if len(records) > 1:
+            log.info(
+                f"Multiple records found in CSV for {csv_s3_uri}. Skipping update"
+            )
+            return (True, payload)
+
+        record = records[0]
+
+    field_blacklist = ["climb_id", "run_index", "run_id", "biosample_id"]
+
+    update_fields = {}
+
+    for k, v in record.items():
+        if k in field_blacklist:
+            continue
+
+        update_fields[k] = v
+
+    update_failure, update_alert, payload = onyx_update(
+        payload=payload, fields=update_fields, log=log
+    )
+
+    payload.pop("climb_id")
+    payload.pop("files")
+    payload.pop("uuid")
+    payload.pop("artifact")
+
+    s3_credentials = get_s3_credentials()
+
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=s3_credentials.endpoint,
+        aws_access_key_id=s3_credentials.access_key,
+        aws_secret_access_key=s3_credentials.secret_key,
+    )
+
+    payload["update_status"] = "failed" if update_failure else "success"
+
+    s3_client.put_object(
+        Bucket=f"{parsed_bucket_name['project']}-{parsed_bucket_name['site_str']}-results",
+        Key=f"{parsed_bucket_name['project']}.{run_index}.{run_id}.update.json",
+        Body=json.dumps(payload),
+    )
+
+    log.info(f"Update status for {parsed_bucket_name['project']}.{run_index}.{run_id}: {payload['update_status']}")
+    return (True, payload)
 
 
 def run(args):
@@ -174,15 +362,6 @@ def run(args):
         with open(os.getenv("ROZ_CONFIG_JSON"), "r") as f:
             config_dict = json.load(f)
 
-        s3_credentials = get_s3_credentials()
-
-        s3_client = boto3.client(
-            "s3",
-            endpoint_url=s3_credentials.endpoint,
-            aws_access_key_id=s3_credentials.access_key,
-            aws_secret_access_key=s3_credentials.secret_key,
-        )
-
         while True:
 
             message = varys_client.receive(
@@ -195,162 +374,21 @@ def run(args):
                 fh.write(str(time.time_ns()))
 
             if message:
-                record = message["Records"][0]
 
-                bucket_name = record["s3"]["bucket"]["name"]
+                parsed_message = json.loads(message.body)
 
-                parsed_bucket_name = {
-                    x: y
-                    for x, y in zip(
-                        ("project", "site_str", "platform", "test_flag"),
-                        bucket_name.split("-"),
-                    )
-                }
-
-                if "." in parsed_bucket_name["site_str"]:
-                    site = parsed_bucket_name["site_str"].split(".")[-2]
-                else:
-                    site = parsed_bucket_name["site_str"]
-
-                # ignore files from test buckets
-                if parsed_bucket_name["test_flag"] == "test":
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                # Skip new files from projects which do not have csv_updates enabled
-                if not config_dict["configs"][parsed_bucket_name["project"]][
-                    "csv_updates"
-                ]:
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                extension, parsed_object_key = parse_object_key(
-                    object_key=record["s3"]["object"]["key"],
+                success, payload = csv_update(
+                    parsed_message=parsed_message,
                     config_dict=config_dict,
-                    project=parsed_bucket_name["project"],
-                    platform=parsed_bucket_name["platform"],
-                    site=site,
-                )
-
-                if not extension or not parsed_object_key:
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                if extension != ".csv":
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                run_index_fail, run_index = onyx_identify_simple(
-                    identifier=parsed_object_key["run_index"],
-                    identity_field="run_index",
-                    project=parsed_bucket_name["project"],
-                    site=site,
                     log=log,
                 )
 
-                if run_index_fail:
-                    varys_client.nack_message(message)
-                    continue
-
-                run_id_fail, run_id = onyx_identify_simple(
-                    identifier=parsed_object_key["run_id"],
-                    identity_field="run_id",
-                    project=parsed_bucket_name["project"],
-                    site=site,
-                    log=log,
-                )
-
-                if run_id_fail:
-                    varys_client.nack_message(message)
-                    continue
-
-                if not all([run_id, run_index]):
+                if success:
                     varys_client.acknowledge_message(message)
                     continue
 
-                climb_id_fail, climb_id = onyx_climb_identify(
-                    run_index=run_index,
-                    run_id=run_id,
-                    project=parsed_bucket_name["project"],
-                    log=log,
-                )
-
-                if climb_id_fail:
-                    varys_client.nack_message(message)
-                    continue
-
-                if not climb_id:
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                payload = {
-                    "climb_id": climb_id,
-                    "project": parsed_bucket_name["project"],
-                    "run_index": run_index,
-                    "run_id": run_id,
-                    "site": site,
-                }
-
-                field_check_status, alert, payload = csv_field_checks(payload=payload)
-
-                if alert:
-                    varys_client.nack_message(message)
-                    continue
-
-                if not field_check_status:
-                    varys_client.acknowledge_message(message)
-                    continue
-
-                csv_s3_uri = f"s3://{bucket_name}/{record['s3']['object']['key']}"
-
-                with open(s3_to_fh(csv_s3_uri, record["s3"]["object"]["eTag"])) as fh:
-                    reader = csv.DictReader(fh)
-
-                    records = [record for record in reader]
-
-                    if len(records) > 1:
-                        log.error(
-                            f"Multiple records found in CSV for {csv_s3_uri}. Skipping update"
-                        )
-                        varys_client.acknowledge_message(message)
-                        continue
-
-                    record = records[0]
-
-                field_blacklist = ["climb_id", "run_index", "run_id", "biosample_id"]
-
-                update_fields = {}
-
-                for k, v in record.items():
-                    if k in field_blacklist:
-                        continue
-
-                    update_fields[k] = v
-
-                update_success, update_alert, payload = onyx_update(
-                    payload=payload, fields=update_fields, log=log
-                )
-
-                if update_success:
-                    payload["update_status"] = "success"
-                    payload.pop("climb_id")
-
-                    s3_client.put_object(
-                        Bucket=f"{parsed_bucket_name['project']}-{parsed_bucket_name['site_str']}-results",
-                        Key=f"{parsed_bucket_name['project']}.{run_index}.{run_id}.update.json",
-                        Body=json.dumps(payload),
-                    )
-
-                else:
-                    payload["update_status"] = "failed"
-
-                    s3_client.put_object(
-                        Bucket=f"{parsed_bucket_name['project']}-{parsed_bucket_name['site_str']}-results",
-                        Key=f"{parsed_bucket_name['project']}.{run_index}.{run_id}.update.json",
-                        Body=json.dumps(payload),
-                    )
-
-                varys_client.acknowledge_message(message)
+                log.error(f"Failed to process message: {json.loads(message.body)}")
+                varys_client.nack_message(message)
 
     except BaseException:
         os.remove("/tmp/healthy")
